40d78970f47954bdf0e14a36c33681383f4f8bf2
Reeves Chiu <reeveschiu@gmail.com>
2021-10-26 14:37:14 +0800

    Added more data and fix main insert

--- insertion_Customer.sql
+++ insertion_Customer.sql
@@ -8,4 +8,10 @@ VALUES
     ('Emily Soto', 86913536 ,'spiritedIcecream8','EmilySoto@gmail.com','767 Rios Summit Suite 255 New Seanchester, MA 79836','Ofv6L&*8tS&id'),
     ('Michael Ponce',87925784,'adoringHeron8','MichaelPonce@yahoo.com','7952 Dawson Plains Suite 895 South Darrell, KS 13800','1p(uAcPQL))'),
     ('Michael Davis',98322225,'trustingLemur8','MichaelDavis@outlook.com','847 Brenda Rapids West Tiffanyland, LA 20789','25LZKRF!BS'),
+    ('Sarah King',83170433,'eagerTomatoe9','SarahKing@yahoo.com','4492 Joshua Flat Mitchellstad, WI 12865','Jf@m7xHi9por79P'),
+    ('Devin Williams',91244704,'cheerfulTeal9','DevinWilliams@outlook.com','204 Olson Rest Apt. 994 New Erinside, KY 25755','vEC9dF&H5G%z'),
+    ('Michael Robinson',93832946,'jumpyWildfowl7','MichaelRobinson@gmail.com','428 Sandra Gateway Suite 572 East Madisonchester, OR 77000','oQ2dL6lLUUW7*g'),
+    ('Clinton Murphy',83141710,'cautiousGranola4','ClintonMurphy@yahoo.com','620 Hunter Motorway Stevensburgh, VA 28482','xAui&ofm41^sK'),
+    ('Stephanie Williams',85431759,'similarMuesli6','StephanieWilliams@outlook.com','94056 Rodriguez Port New Rebeccaberg, WY 36272','*n^EhUXhre&Orqi'),
+    ('Shane Edwards',86875376,'mercifulDoughnut4','ShaneEdwards@gmail.com','708 Carr Burg Apt. 944 Bradleymouth, ND 94426','*0A*hA(CM('),
     ('Philip Rogers',97344507,'decimalDinosaur1','PhilipRogers@gmail.com','59803 Jones Street Chadtown, SD 89378','5JUS6OngZZCeEg');
